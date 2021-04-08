f12c97833dc65c13c3e3591742b7980b8758ffdb
Conor <conor.okeefe.2121@gmail.com>
2021-04-07 19:35:41 -0700

    Average.py Assignment 2.a

---
+++ Average.py
@@ -0,0 +1,10 @@
+# Author: Conor Smith
+# Date: 4/6/2021
+# Description: Asks the user for five numbers and then prints out the average of those numbers.
+print("Enter Five Numbers.")
+num_1 = float(input())
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
+print("The average of those numbers is:", (num_1 + num_2 + num_3 + num_4 + num_5) / 5)
