6527a8367f7c7cd442cd9bd3649f9bab79cd1453
Conor <conor.okeefe.2121@gmail.com>
2021-04-07 20:50:19 -0700

    Average.py

--- Average.py
+++ Average.py
@@ -2,9 +2,9 @@
 # Date: 4/6/2021
 # Description: Asks the user for five numbers and then prints out the average of those numbers.
 print("Enter Five Numbers.")
-num_1 = float(input())
-num_2 = float(input())
-num_3 = float(input())
-num_4 = float(input())
-num_5 = float(input())
+num_1 = int(input())
+num_2 = int(input())
+num_3 = int(input())
+num_4 = int(input())
+num_5 = int(input())
 print("The average of those numbers is:", (num_1 + num_2 + num_3 + num_4 + num_5) / 5)
